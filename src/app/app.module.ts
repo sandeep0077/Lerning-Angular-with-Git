dc4adbb8083a40d991eb203d971aaf1851d7d454
Sandeep <sm671903@gmail.com>
2021-11-06 10:04:55 +0530

    PART-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component'; // and type script knows about server component
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent // now angulr knows about server component
   ],
   imports: [
-    BrowserModule
+    BrowserModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
