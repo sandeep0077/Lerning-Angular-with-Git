90edeb125e872d7670315b6927040b6c763c35aa
Sandeep <sm671903@gmail.com>
2021-11-06 15:02:02 +0530

    Two Way Data Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 allowNewServer = false;
 serverCreationStatus =  "Server was not Created";
-serverName = "";
+serverName = "Test Server";
 
 // Event Binding
 onCreateServer() {
